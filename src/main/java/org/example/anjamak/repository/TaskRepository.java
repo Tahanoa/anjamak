4e43c2973ed6371945b2bfe1b6ddf1284640824a
Taha <144627369+Tahanoa@users.noreply.github.com>
2025-09-27 17:29:36 +0330

    add task Repository

---
+++ src/main/java/org/example/anjamak/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package org.example.anjamak.repository;
+
+import org.example.anjamak.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    Page<Task> findByTitleContaining(String title, Pageable pageable);
+    Page<Task> findByCompleted(boolean completed, Pageable pageable);
+}
