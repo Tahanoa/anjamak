a3e4fae757a67ba91602c141689a90a948f75945
Taha <144627369+Tahanoa@users.noreply.github.com>
2025-09-27 17:30:11 +0330

    add task Service

---
+++ src/main/java/org/example/anjamak/service/TaskService.java
@@ -0,0 +1,14 @@
+package org.example.anjamak.service;
+
+import org.example.anjamak.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface TaskService {
+    void addTask(Task task);
+    void updateTask(Task task);
+    void deleteTask(Task task);
+    Task getTask(int id);
+    Page<Task> getTasks(Pageable pageable);
+    Page<Task> findByTitle(String title, Pageable pageable);
+}
