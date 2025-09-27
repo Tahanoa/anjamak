daaa32eb3263dec5014f548744174cab16171ef7
Taha <144627369+Tahanoa@users.noreply.github.com>
2025-09-27 16:37:21 +0330

    first commit

---
+++ src/main/java/org/example/anjamak/AnjamakApplication.java
@@ -0,0 +1,13 @@
+package org.example.anjamak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnjamakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnjamakApplication.class, args);
+    }
+
+}
