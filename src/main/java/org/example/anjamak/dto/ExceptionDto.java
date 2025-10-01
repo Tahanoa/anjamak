b0fb35e7b1e768e0e3e958bf78acac14c013bb62
Taha <144627369+Tahanoa@users.noreply.github.com>
2025-10-01 21:28:43 +0330

    turn class ExceptionDto into record

--- src/main/java/org/example/anjamak/dto/ExceptionDto.java
+++ src/main/java/org/example/anjamak/dto/ExceptionDto.java
@@ -1,27 +1,6 @@
 package org.example.anjamak.dto;
 
-public class ExceptionDto {
-    private String code;
-    private String message;
-
-    public ExceptionDto(String code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-}
+public record ExceptionDto(
+        String code,
+        String message
+) {}
